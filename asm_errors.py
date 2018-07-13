fe3ab09b3364ade522d5f91d754025b5168ccb97
James Petullo <pentester18@gmail.com>
2018-07-13 15:35:11 -0400

    Update asm_errors.py

--- asm_errors.py
+++ asm_errors.py
@@ -1,7 +1,14 @@
-
-
 class InvalidRegister(Exception):
     pass
 
 class VariableNotDeclared(Exception):
+    pass
+
+class InvalidMovDestination(Exception):
+    pass
+
+class InvalidIndexAccess(Exception):
+    pass
+
+class InvalidAsmFilename(Exception):
     pass
